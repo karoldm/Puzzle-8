f7dce6aca69f4306a609c7a7c7506021a75b7505
karol marques <karoldm1579@gmail.com>
2022-11-19 14:35:18 -0300

    handle repeted movements

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 //Desenvolvido por Karolyne Domiciano Marques para a disciplina de Intenligência Artificial
 
 const board = document.querySelector("#board");
-const heuristic01 = document.querySelector("#heuristic01");
-const heuristic02 = document.querySelector("#heuristic02");
-const heuristicP = document.querySelector("#heuristicP");
+const heuristicOneButton = document.querySelector("#heuristic01");
+const heuristicTwoButton = document.querySelector("#heuristic02");
+const heuristicPButton = document.querySelector("#heuristicP");
 
 let lastMove = [];
 
-heuristic01.addEventListener("click", () => {
+heuristicOneButton.addEventListener("click", () => {
     mixSquaresRandom();
+    heuristicOne();
 });
-heuristic02.addEventListener("click", () => {
+heuristicTwoButton.addEventListener("click", () => {
     mixSquaresRandom();
+    heuristicTwo();
 });
-heuristicP.addEventListener("click", () => {
+heuristicPButton.addEventListener("click", () => {
     mixSquaresRandom();
+    heuristicP();
 });
 
 //matriz para calcular heuristicas
@@ -29,6 +32,18 @@ let positionSquares;
 //posição do quadrado 9 na matriz inicialmente
 let y;
 let x;
+
+function heuristicOne() {
+
+}
+
+function heuristicTwo() {
+
+}
+
+function heuristicP() {
+
+}
 
 
 function init() {
@@ -66,9 +81,6 @@ function delay(milliseconds) {
 //Mover o quadrado 9 (quadrado vazio) para uma nova posição 
 //Troca o quadrado 9 pelo quadrado na posição newy newx
 function moveSquare(newy, newx) {
-    //armazenando ultimos movimentos para identificar loops nos movimentos
-    lastMove.push(matriz[newy][newx]);
-
 
     //movendo quadrado 9 para posição do seu vizinho
     matrizSquares[y][x].style.left = `${(150) * (newx - x) + positionSquares[y][x].x}px`;
@@ -92,16 +104,30 @@ function moveSquare(newy, newx) {
 
 //Função para verificar loops nas jogadas
 function loop() {
-    for (salto = 1; salto < 6; salto++) {
-        for (i = 0; i < matriz.length; i++) {
-            for (j = 0; j < matriz[i].length; j++) {
-                //[1 2 3 1 2 3]
-                //salto = 2 --> 12 != 31, 23 != 12, 31 != 23
-                //salto = 3 --> 123 == 123 return true! 
 
-            }
+    if (lastMove.length === 0 || lastMove.length === 1) return false;
+
+    if (lastMove[lastMove.length - 1] === lastMove[lastMove.length - 2]) return true;
+
+    let subMovs1;
+    let subMovs2;
+
+    for (jump = 2; jump < 6; jump++) {
+        subMovs1 = lastMove.slice(0, jump).join('');
+        for (i = jump; i < lastMove.length; i++) {
+            //[1, 2, 1, 2, 1, 2]
+            //jump = 2
+            //subMovs1 = 12 --> lastMove[0], lastMove[1]
+            //subMovs2 = 12 --> lastMove[2], lastMove[3]
+            //if(subMovs1 === subMovs2) return true
+
+            subMovs2 = lastMove.slice(i, i + jump).join('');
+
+            if (subMovs1 === subMovs2) return true;
+            subMovs1 = subMovs2;
         }
     }
+
     return false;
 }
 
@@ -115,35 +141,76 @@ function randomNeighbor() {
 
     //verificando posição atual do quadrado 9 e armazenando a posição dos seus vizinhos
     if (y == 2 && x == 2) {
-        neighbors.push({ 'y': y - 1, 'x': x }, { 'y': y, 'x': x - 1 });
+        neighbors.push(
+            { 'y': y - 1, 'x': x },
+            { 'y': y, 'x': x - 1 });
     }
     else if (y == 2 && x == 1) {
-        neighbors.push({ 'y': y - 1, 'x': x }, { 'y': y, 'x': x - 1 }, { 'y': y, 'x': x + 1 });
+        neighbors.push(
+            { 'y': y - 1, 'x': x },
+            { 'y': y, 'x': x - 1 },
+            { 'y': y, 'x': x + 1 });
     }
     else if (y == 2 && x == 0) {
-        neighbors.push({ 'y': y - 1, 'x': x }, { 'y': y, 'x': x + 1 });
+        neighbors.push(
+            { 'y': y - 1, 'x': x },
+            { 'y': y, 'x': x + 1 });
     }
     else if (y == 1 && x == 2) {
-        neighbors.push({ 'y': y - 1, 'x': x }, { 'y': y + 1, 'x': x }, { 'y': y, 'x': x - 1 });
+        neighbors.push(
+            { 'y': y - 1, 'x': x },
+            { 'y': y + 1, 'x': x },
+            { 'y': y, 'x': x - 1 });
     }
     else if (y == 1 && x == 1) {
-        neighbors.push({ 'y': y - 1, 'x': x }, { 'y': y + 1, 'x': x }, { 'y': y, 'x': x - 1 }, { 'y': y, 'x': x + 1 });
+        neighbors.push(
+            { 'y': y - 1, 'x': x },
+            { 'y': y + 1, 'x': x },
+            { 'y': y, 'x': x - 1 },
+            { 'y': y, 'x': x + 1 });
     }
     else if (y == 1 && x == 0) {
-        neighbors.push({ 'y': y - 1, 'x': x }, { 'y': y + 1, 'x': x }, { 'y': y, 'x': x + 1 });
+        neighbors.push(
+            { 'y': y - 1, 'x': x },
+            { 'y': y + 1, 'x': x },
+            { 'y': y, 'x': x + 1 });
     }
     else if (y == 0 && x == 2) {
-        neighbors.push({ 'y': y + 1, 'x': x }, { 'y': y, 'x': x - 1 });
+        neighbors.push(
+            { 'y': y + 1, 'x': x },
+            { 'y': y, 'x': x - 1 });
     }
     else if (y == 0 && x == 1) {
-        neighbors.push({ 'y': y + 1, 'x': x }, { 'y': y, 'x': x - 1 }, { 'y': y, 'x': x + 1 });
+        neighbors.push(
+            { 'y': y + 1, 'x': x },
+            { 'y': y, 'x': x - 1 },
+            { 'y': y, 'x': x + 1 });
     }
     else if (y == 0 && x == 0) {
-        neighbors.push({ 'y': y + 1, 'x': x }, { 'y': y, 'x': x + 1 });
+        neighbors.push(
+            { 'y': y + 1, 'x': x },
+            { 'y': y, 'x': x + 1 });
     }
 
     //escolhendo um vizinho aleatório
-    const neighbor = Math.floor(Math.random() * ((neighbors.length - 1) - 0) + 0);
+    let neighbor = Math.floor(Math.random() * ((neighbors.length - 1) - 0) + 0);
+
+    //armazenando ultimos movimentos para identificar loops nos movimentos
+    lastMove.push(matriz[neighbors[neighbor].y][neighbors[neighbor].x]);
+
+    while (loop()) {
+        //removendo vizinho já que ele mover para ele levará a um loop
+        neighbors = neighbors.filter((e) => e !== neighbor);
+
+        //removendo ultimo movimento já que ele levará a um loop
+        lastMove.pop();
+
+        //escolhendo um vizinho aleatório
+        neighbor = Math.floor(Math.random() * (neighbors.length));
+
+        //armazenando ultimos movimentos para identificar loops nos movimentos
+        lastMove.push(matriz[neighbors[neighbor].y][neighbors[neighbor].x]);
+    }
 
     //movendo quadrado 9 para a posição do seu vizinho
     moveSquare(neighbors[neighbor].y, neighbors[neighbor].x);
